0413c3ce6bf08f41f72c879b3f2eba79e33881c6
bkwag <kwag93@naver.com>
2022-02-25 15:23:57 +0900

    fix: 해상도 알람창 부분을 수정하여 리렌더링 되지 않도록 함

--- src/common/DraggableWindow.tsx
+++ src/common/DraggableWindow.tsx
@@ -20,6 +20,12 @@ const StyledWindow = styled.div`
   border-radius: 8px;
   border: 1px solid #beb5b4;
   background-color: #eeeeee;
+  @media (max-width: 1080px) {
+    display: none;
+  }
+  @media (max-height: 700px) {
+    display: none;
+  }
 
   .card {
     width: 100%;
